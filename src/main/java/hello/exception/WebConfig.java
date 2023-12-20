91319242085271e2ee0bc69eee108b00e081ddbd
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-12-20 18:25:49 +0900

    feat: 오류 발생 시 intercepter 중복 호출 방지 로직
    intercepter는 필터와 다르게 DispatcherType을 지정하지 않아도 excludePathPatterns에 경로를 작성하면 된다.
    intercetper를 거치지 않게 하려면 excludePathPatterns에 경로를 추가하면 됨.

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -11,7 +13,17 @@ import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                //filter와 다르게 인터셉터는 오류 페이지와 관련된 path를 제외해주면 된다.
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());

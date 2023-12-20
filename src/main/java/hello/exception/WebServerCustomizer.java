eb076dbb385523b87fdecd2e66e59d4bb981444d
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-12-20 15:43:09 +0900

    feat: 서블릿 오류 페이지 등록 및 오류 페이지 별 매핑 로직
    WebServerCustomizer를 통해 오류 코드에 맞는 경로를 설정하고, ErrorPageController를 통해서 오류 코드에 맞는 페이지를 리턴함.

---
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    // 기본 오류 페이지를 customize 한다.
+    public void customize(ConfigurableWebServerFactory factory) {
+        //ErrorPage(오류 코드, 이동 할 페이지경로) 생성
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+        //에러 페이지 등록. (factory.addErrorPages)
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+
+    }
+}
